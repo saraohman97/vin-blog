a58526db82d3856083802360131371e4e11a5333
Sara Öhman <saraohman97@gmail.com>
2024-10-20 12:24:13 +0200

    Deleted some files and added prisma setup.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+// import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +25,9 @@ export default function RootLayout({
           defaultTheme="light"
           enableSystem
           disableTransitionOnChange
-        > */}
+          > */}
           {children}
+          <Toaster />
         {/* </ThemeProvider> */}
       </body>
     </html>
